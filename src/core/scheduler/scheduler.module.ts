521067f2c62e164d63821df4bffd3e4d1eecf0a9
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-07-02 00:00:59 +0530

    ds

--- src/core/scheduler/scheduler.module.ts
+++ src/core/scheduler/scheduler.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule as NestScheduleModule } from '@nestjs/schedule';
 import { TriggerJobsService } from './trigger.jobs';
+import { WorkflowsModule } from '../../domains/workflows/workflows.module';
+import { ExecutionsModule } from '../../domains/executions/executions.module';
 
 @Module({
   imports: [
-    // Enables @Cron, @Interval, etc.
     NestScheduleModule.forRoot(),
+    WorkflowsModule,
+    ExecutionsModule,
   ],
   providers: [TriggerJobsService],
   exports: [TriggerJobsService],
 })
-export class SchedulerModule {}
+export class SchedulerModule { }
